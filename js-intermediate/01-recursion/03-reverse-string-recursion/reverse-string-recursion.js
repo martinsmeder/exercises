6a9fd1e096d3cbad60562796943b2567df50c614
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-12-23 11:01:56 +0100

    Added solution to sumNumbers recursive function

--- js-intermediate/01-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ js-intermediate/01-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,29 +1,23 @@
-// Step by step explanation:
-// sumUpTo(5) calls sumUpTo(4) and stores 5 to later return the sum of all values between 1 and 5
-// sumUpTo(4) calls sumUpTo(3) and stores 4 to later return the sum of all values between 1 and 4
-// sumUpTo(3) calls sumUpTo(2) and stores 3 to later return the sum of all values between 1 and 3
-// sumUpTo(2) calls sumUpTo(1) and stores 2 to later return the sum of all values between 1 and 2
-// sumUpTo(1) calls sumUpTo(0) and stores 1 to later return the sum of all values between 1 and 1
-// sumUpTo(0) returns 0 (base case and not computed because the value for 0 is known)
-// sumUpTo(1) returns 1 (computed using the stored value from the last sumUpTo(1) call on the call stack))
-// sumUpTo(2) returns 3 (computed using the stored value from the last sumUpTo(2) call on the call stack)
-// sumUpTo(3) returns 6 (computed using the stored value from the last sumUpTo(3) call on the call stack)
-// sumUpTo(4) returns 10 (computed using the stored value from the last sumUpTo(4) call on the call stack)
-// sumUpTo(5) returns 15 (computed using the stored value from the last sumUpTo(5) call on the call stack)
+// // Sum numbers:
+// let numbers = [1, 2, 3, 4, 5];
 
-// function sumUpTo(n) {
-//   if (n <= 0) {
-//     return 0;
-//   }
-//   return n + sumUpTo(n - 1);
+// function sumArray(arr) {
+//   // Base case: empty array
+//   if (arr.length === 0) return 0;
+
+//   // Recursive case: Non-empty array
+//   const [first, ...rest] = arr; // Get first element, and rest of array
+//   const remainingElements = sumArray(rest); // Pass rest of array to recursive call
+//   return first + remainingElements; // Combine sum of first element with sum of remaining elements
 // }
+
+// console.log(sumArray(numbers)); // 15
+
+// ===========================================================================================
 
 // Reverse string:
-// function reverseString(str) {
-// ???
-// }
-
-// Sum numbers:
-function sumArray(arr) {}
+function reverseString(str) {
+  // ???
+}
 
 module.exports = reverseString;
