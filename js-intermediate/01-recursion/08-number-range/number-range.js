0f122d3ec0cecbcce6a61298ebc872cb51edd24c
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-12-30 13:31:46 +0100

    Added solution and explanation to numberRange()

--- js-intermediate/01-recursion/08-number-range/number-range.js
+++ js-intermediate/01-recursion/08-number-range/number-range.js
@@ -1,3 +1,17 @@
-function numberRange() {}
+function numberRange(x, y) {
+  // Base-case:
+  // When y is equal to x, return an array containing x
+  if (y === x) return [x];
+
+  // Recursive case:
+  // Recursively generate the array of numbers from x to y - 1
+  const numbers = numberRange(x, y - 1);
+  // Add y to array
+  numbers.push(y);
+  // Return the updated array containing the range from x to y
+  return numbers;
+}
+
+console.log(numberRange(3, 10));
 
 module.exports = numberRange;
