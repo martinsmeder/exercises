3d765bd3728e79fd3b61f4f063377adc209d2c1d
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-12-30 11:08:08 +0100

    Added my solution and explanation to factorial

--- js-intermediate/01-recursion/05-factorial/factorial.js
+++ js-intermediate/01-recursion/05-factorial/factorial.js
@@ -1,3 +1,10 @@
-function factorial() {}
+function factorial(num) {
+  // Base-case:
+  if (num === 0) return 1;
+
+  // Multiply current num by previous num (num-1) until reaching
+  // base-case, and return result
+  return num * factorial(num - 1);
+}
 
 module.exports = factorial;
