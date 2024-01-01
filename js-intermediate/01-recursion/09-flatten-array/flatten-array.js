2897cf7b98c65b12ece57334e933f0f7b8adca1b
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2024-01-01 13:33:04 +0100

    Added solution and explanation to flattenArray()

--- js-intermediate/01-recursion/09-flatten-array/flatten-array.js
+++ js-intermediate/01-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,20 @@
-function flattenArray() {}
+function flattenArray(arr) {
+  let result = [];
+
+  // Loop through array
+  arr.forEach((item) => {
+    // If item is array
+    if (Array.isArray(item)) {
+      // Concatenate flattened elements from recursive calls on
+      // nested arrays, and update result
+      result = result.concat(flattenArray(item));
+    } else {
+      // Otherwise, add item to result
+      result.push(item);
+    }
+  });
+
+  return result;
+}
 
 module.exports = flattenArray;
