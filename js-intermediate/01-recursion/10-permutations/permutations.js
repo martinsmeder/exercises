e63a1bb69aec405502ed22bb8a16234921bc9426
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2024-01-02 13:28:38 +0100

    Added partial solution to permutations()

--- js-intermediate/01-recursion/10-permutations/permutations.js
+++ js-intermediate/01-recursion/10-permutations/permutations.js
@@ -1,3 +1,43 @@
-function permutations() {}
+// test('Permutations', () => {
+//   expect(permutations('abc')).toEqual([
+//     'abc',
+//     'acb',
+//     'bac',
+//     'bca',
+//     'cab',
+//     'cba',
+//   ]);
+//   expect(permutations('dog')).toEqual([
+//     'dog',
+//     'dgo',
+//     'odg',
+//     'ogd',
+//     'gdo',
+//     'god',
+//   ]);
+//   expect(permutations('')).toEqual(['']);
+// });
+
+function permutations(str) {
+  const result = [];
+
+  // Base-case:
+  if (str.length === 0) {
+    result.push('');
+    return result;
+  }
+
+  // Recursive case:
+  // ???
+}
 
 module.exports = permutations;
+
+// Base-case:
+// str.length is 0 > return array with empty string
+
+// Recursive case:
+// Loop through string
+// Extract one letter and rest of letters
+// Recursively call permutations(str) on rest of letters
+// Use nested loop
