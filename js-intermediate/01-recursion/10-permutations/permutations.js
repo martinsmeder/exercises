218f2dcfdbafa0b3fd3046b7ec2c49de69451bd2
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2024-01-03 10:25:02 +0100

    Added solution and explanation to permutations()

--- js-intermediate/01-recursion/10-permutations/permutations.js
+++ js-intermediate/01-recursion/10-permutations/permutations.js
@@ -1,23 +1,3 @@
-// test('Permutations', () => {
-//   expect(permutations('abc')).toEqual([
-//     'abc',
-//     'acb',
-//     'bac',
-//     'bca',
-//     'cab',
-//     'cba',
-//   ]);
-//   expect(permutations('dog')).toEqual([
-//     'dog',
-//     'dgo',
-//     'odg',
-//     'ogd',
-//     'gdo',
-//     'god',
-//   ]);
-//   expect(permutations('')).toEqual(['']);
-// });
-
 function permutations(str) {
   const result = [];
 
@@ -28,16 +8,54 @@ function permutations(str) {
   }
 
   // Recursive case:
-  // ???
+  for (let i = 0; i < str.length; i++) {
+    // Get current character
+    const current = str[i];
+    // Get rest of characters
+    const rest = str.slice(0, i) + str.slice(i + 1);
+    // Recursive call with the rest of the string
+    const subPermutation = permutations(rest);
+
+    for (let j = 0; j < subPermutation.length; j++) {
+      // Combine current character with sub-permutations
+      const permutation = current + subPermutation[j];
+      // Add the permutation to the result array
+      result.push(permutation);
+    }
+  }
+
+  return result;
 }
 
 module.exports = permutations;
 
-// Base-case:
-// str.length is 0 > return array with empty string
+// ================================ Example - permutations('abc') ================================
+// First iteration of outer loop:
+// 1. i = 0, current = 'a', rest = 'bc'
+// 2. subPermutation = permutations('bc')
+// 3. i = 0, current = 'b', rest = 'c'
+// Base case reached: [''] (for string 'c')
+// 1. subPermutation = ['c']
+// 2. subPermutation = ['bc', 'cb'] (for string 'bc')
+// 3. subPermutation = ['abc', 'acb'] (for string 'abc')
+// Result array: ['abc', 'acb']
 
-// Recursive case:
-// Loop through string
-// Extract one letter and rest of letters
-// Recursively call permutations(str) on rest of letters
-// Use nested loop
+// Second iteration of outer loop:
+// 4. i = 1, current = 'b', rest = 'ac'
+// 5. subPermutation = permutations('ac')
+// 6. i = 0, current = 'a', rest = 'c'
+// Base case reached: [''] (for string 'c')
+// 4. subPermutation = ['c']
+// 5. subPermutation = ['ac', 'ca'] (for string 'ac')
+// 6. subPermutation = ['bac', 'bca'] (for string 'bac')
+// Result array: ['abc', 'acb', 'bac', 'bca']
+
+// Third iteration of outer loop:
+// 7. i = 2, current = 'c', rest = 'ab'
+// 8. subPermutation = permutations('ab')
+// 9. i = 0, current = 'a', rest = 'b'
+// Base case reached: [''] (for string 'b')
+// 7. subPermutation = ['b']
+// 8. subPermutation = ['ab', 'ba'] (for string 'ab')
+// 9. subPermutation = ['cab', 'cba'] (for string 'cab')
+// Result array: ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
