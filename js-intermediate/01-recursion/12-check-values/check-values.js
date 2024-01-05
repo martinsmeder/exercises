bda75f6e14c902830821c861beb8008c6c7d0457
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2024-01-05 11:45:10 +0100

    Added new exercise checkValues()

---
+++ js-intermediate/01-recursion/12-check-values/check-values.js
@@ -0,0 +1,11 @@
+// Check all values in an array:
+// Write a function called "all" which accepts an array and a
+// callback and returns true if every value in the array returns
+// true when passed as parameter to the callback function
+
+function checkValues(arr, callback) {
+  if (arr.length === 0) return true;
+}
+
+// Base-case:
+// Empty array > return true
