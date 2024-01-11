ff363449c3f4911146ffa340b00cd4160924babc
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2024-01-11 11:38:29 +0100

    Added sum-squares.js

---
+++ js-intermediate/01-recursion/16-sum-squares/sum-squares.js
@@ -0,0 +1,18 @@
+// Sum squares:
+// Write a function that sums squares of numbers in an array that
+// may contain more arrays
+
+// Examples:
+// console.log(SumSquares([1,2,3])); // 1 + 4 + 9 = 14
+// console.log(SumSquares([[1,2],3])); // 1 + 4 + 9 = 14
+// console.log(SumSquares([[[[[[[[[1]]]]]]]]])); // 1 = 1
+// console.log(SumSquares([10,[[10],10],[10]])); // 100 + 100 + 100 + 100 = 400
+
+function sumSquares(arr) {}
+
+console.log(sumSquares([[1, 2], 3]));
+
+module.exports = sumSquares;
+
+// Base-cases:
+// If arr.length is 0 > return 0
