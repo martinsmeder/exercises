9ea00d7863bc1239a5b4c30d82831f4d5939eb26
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2024-01-20 14:22:22 +0100

    Solution and explanation to phoneNumberDirectory()

--- js-intermediate/03-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ js-intermediate/03-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,35 @@
-function phoneNumberDirectory() {}
+// Phone Number Directory:
+// You are building a phone number directory application. Implement a
+// function called `phoneNumberDirectory` that takes an array of phone
+// numbers as input and returns a Map with names as keys and
+// corresponding phone numbers as values.
+
+// Example:
+// const phoneNumbers = [
+//   'John:123-456-7890',
+//   'Jane:987-654-3210',
+//   'Joe:555-555-5555',
+// ];
+// console.log(phoneNumberDirectory(phoneNumbers));
+// Output: Map { 'John' => '123-456-7890', 'Jane' => '987-654-3210', 'Joe' => '555-555-5555' }
+
+// Constraints:
+// he input array elements should be formatted as `NAME:PHONENUMBER`
+
+function phoneNumberDirectory(arr) {
+  // Initiate empty map
+  let map = new Map();
+
+  // Loop though array
+  arr.forEach((string) => {
+    // Split each string into name and number
+    const [name, number] = string.split(':');
+    // Add name and corresponding number to map
+    map.set(name, number);
+  });
+
+  // Return updated map
+  return map;
+}
 
 module.exports = phoneNumberDirectory;
