747223e48a5312b38e66cf91550cfb08278300e9
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2024-01-24 18:49:52 +0100

    Solution + explanation to reverseStringStack()

--- js-intermediate/04-stacks-queues-linked-lists/02-stack-implementation/stack.js
+++ js-intermediate/04-stacks-queues-linked-lists/02-stack-implementation/stack.js
@@ -1,3 +1,56 @@
-class Stack {}
+class Stack {
+  constructor() {
+    this.maxSize = 100;
+    this.stack = [];
+    this.top = -1;
+  }
+
+  push(value) {
+    if (this.isFull()) {
+      return false;
+    }
+    this.top++;
+    this.stack[this.top] = value;
+
+    return true;
+  }
+
+  isFull() {
+    return this.top === this.maxSize - 1;
+  }
+
+  isEmpty() {
+    return this.top === -1;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    this.top--;
+
+    return this.stack.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.stack[this.top];
+  }
+}
+
+// const stack = new Stack();
+
+// stack.push(1);
+// stack.push(2);
+// stack.push(3);
+
+// console.log(stack); // Stack {maxSize: 100, stack: Array(3), top: 2}
+
+// stack.pop();
+// console.log(stack.peek()); // 2
+// stack.pop();
+// console.log(stack.peek()); // 1
 
 module.exports = Stack;
--- js-intermediate/04-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ js-intermediate/04-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,32 @@
+// Reverse String Using a Stack:
+// Write a function called reverseStringStack that takes in a string
+// and returns the reversed version of the string. Be sure to use the
+// Stack class that we created.
+
+// Constraints:
+// The string will only contain lowercase letters and spaces
+
+// reverseStringStack('hello'); // olleh
+// reverseStringStack('Howdy'); // ydwoH
+// reverseStringStack('Greetings from Earth'); // htraE morf sgniteerG
+
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+function reverseStringStack(str) {
+  // Initiate reversed variable as empty string
+  let reversed = '';
+
+  // Initiate empty stack
+  const stack = new Stack();
+
+  // Push all letters and spaces to stack
+  for (let i = 0; i < str.length; i++) stack.push(str[i]);
+
+  // Add each letter/space to reversed, from top to bottom
+  for (let j = 0; j < str.length; j++) reversed += stack.pop();
+
+  // Return reversed string
+  return reversed;
+}
 
 module.exports = reverseStringStack;
