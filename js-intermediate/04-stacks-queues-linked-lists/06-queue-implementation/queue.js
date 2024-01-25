d7ecc7dccba8402121774749d2ed57f286fe2054
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2024-01-25 18:34:30 +0100

    Solution + explanation to reverseStringWithQueue()

--- js-intermediate/04-stacks-queues-linked-lists/06-queue-implementation/queue.js
+++ js-intermediate/04-stacks-queues-linked-lists/06-queue-implementation/queue.js
@@ -1,3 +1,41 @@
-class Queue {}
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.head = 0;
+    this.tail = 0;
+    this.maxSize = 100;
+  }
+
+  getLength() {
+    return this.tail - this.head;
+  }
+
+  isEmpty() {
+    return this.getLength() === 0;
+  }
+
+  isFull() {
+    return this.getLength() === this.maxSize;
+  }
+
+  enqueue(element) {
+    if (this.isFull()) {
+      return false;
+    }
+    this.queue[this.tail] = element;
+    this.tail++;
+    return true;
+  }
+
+  dequeue() {
+    const item = this.queue[this.head];
+    this.head++;
+    return item;
+  }
+
+  peek() {
+    return this.queue[this.head];
+  }
+}
 
 module.exports = Queue;
--- js-intermediate/04-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ js-intermediate/04-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,33 @@
+// Reverse a String Using a Queue:
+// Write a function called reverseStringQueue that takes in a string
+// and returns the reverse of that string.
+
+// The function should return the reversed string, but I want you to
+// use the Queue class to do it.
+
+// Constraints:
+// The string will only contain lowercase letters and spaces
+
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+  // Initialize an empty string to store the reversed result
+  let reversed = '';
+  // Create a new Queue instance
+  const queue = new Queue();
+
+  // Enqueue each character of the original string in reverse order
+  for (let i = str.length - 1; i >= 0; i--) {
+    queue.enqueue(str[i]);
+  }
+
+  // Dequeue each character from the queue and append it to the reversed string
+  for (let j = 0; j < str.length; j++) {
+    reversed += queue.dequeue();
+  }
+
+  // Return the reversed string
+  return reversed;
+};
 
 module.exports = reverseStringWithQueue;
