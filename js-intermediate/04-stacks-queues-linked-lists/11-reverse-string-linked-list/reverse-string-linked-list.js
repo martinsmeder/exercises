d476a3d8144765f98dbf4421933e800a032976da
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2024-01-26 20:37:20 +0100

    Solution + explanation to reverseStringLinkedList()

--- js-intermediate/04-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
+++ js-intermediate/04-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
@@ -1,5 +1,41 @@
+// Reverse String Using Linked List:
+// Write a function called reverseStringLinkedList that takes in a
+// string and returns the reverse of that string.
+
+// The function should return the reversed string, but I want you to
+// use the LinkedList class to do it.
+
+// Examples:
+// reverseStringQueue('hello'); // olleh
+// reverseStringQueue('Howdy'); // ydwoH
+// reverseStringQueue('Greetings from Earth'); // htraE morf sgniteerG
+
 const { LinkedList } = require('./linked-list');
 
-function reverseStringLinkedList() {}
+function reverseStringLinkedList(str) {
+  // Initiate empty linked list
+  let list = new LinkedList();
+  // Initiate reversed accumulator to empty string
+  let reversed = '';
+  // Initiate index at the end of the original string
+  let index = str.length - 1;
+
+  // Loop though string
+  for (let i = 0; i < str.length; i++) {
+    // Add each letter to list
+    list.add(str[i]);
+  }
+
+  // Loop through string again
+  for (let j = 0; j < str.length; j++) {
+    // Accumulate letters starting from end of list
+    reversed += list.get(index);
+    // Decrement index by 1 to get the rest of the letters
+    index--;
+  }
+
+  // Return reversed string
+  return reversed;
+}
 
 module.exports = reverseStringLinkedList;
