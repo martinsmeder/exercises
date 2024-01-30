58c52054419c8f482a3ef7b8efc05708df5b1b1a
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2024-01-30 21:02:13 +0100

    Solution + explanation to maximumDepth()

--- js-intermediate/05-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
+++ js-intermediate/05-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
@@ -1,8 +1,45 @@
-const Queue = require('./queue');
+// Create a binary tree:      a
+//                          /   \
+//                         b     c
+//                        / \    /
+//                       d   e  f
 
-class Node {}
+const Queue = require("./queue");
 
-function breadthFirstTraversal() {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function breadthFirstTraversal(root) {
+  // If no root node, return empty array
+  if (!root) return [];
+
+  // Initiate empty result and queue arrays
+  const result = [];
+  const queue = new Queue();
+
+  // Push root node to queue
+  queue.enqueue(root);
+
+  // Loop though queue as long as it's not empty
+  while (!queue.isEmpty()) {
+    // Get current (first) element from queue
+    const current = queue.dequeue();
+    // Push current data into results array
+    result.push(current.data);
+
+    // If left and/or right child exists, push them to the queue
+    if (current.left) queue.enqueue(current.left);
+    if (current.right) queue.enqueue(current.right);
+  }
+
+  // Return results array
+  return result;
+}
 
 module.exports = {
   Node,
--- js-intermediate/05-binary-trees-graphs/06-maximum-depth/maximum-depth.js
+++ js-intermediate/05-binary-trees-graphs/06-maximum-depth/maximum-depth.js
@@ -1,6 +1,55 @@
-class Node {}
+// Binary Tree Maximum Depth:
+// Write a function called maxDepth that takes the root of a binary tree
+// as input and returns the maximum depth of the tree. The maximum depth is
+// defined as the number of nodes along the longest path from the root node
+// down to the farthest leaf node.
 
-function maxDepth() {}
+// Examples:
+// Explanation: The binary tree is as follows:
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// The maximum depth is 3, which is the path 3 -> 20 -> 7.
+
+// Explanation: The binary tree is as follows:
+//     1
+//      \
+//       2
+// The maximum depth is 2, which is the path 1 -> 2.
+
+// Input: root = []
+// Output: 0
+// Explanation: An empty tree has a maximum depth of 0.
+
+// Hints
+// 1. You can solve this problem using a depth-first traversal approach.
+// 2. Use recursion to explore the left and right subtrees of each node and
+//    return the maximum depth between them.
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function maxDepth(root) {
+  // Base case: If no root, end of branch is reached, return null
+  if (!root) return 0;
+
+  // Get leftDepth and rightDepth
+  let leftDepth = maxDepth(root.left);
+  let rightDepth = maxDepth(root.right);
+
+  // If left is deeper, return leftDepth + 1 (for current level)
+  if (leftDepth > rightDepth) return leftDepth + 1;
+  // If right is deeper, or they have equal length, return rightDepth + 1
+  // (for current level)
+  else return rightDepth + 1;
+}
 
 module.exports = {
   maxDepth,
